770b059ea1a0c13bde42a63f6a3144eaa3e67f46
Luis Cordoba <luis.cordoba@perficient.com>
2022-12-20 17:09:52 -0500

    End section 3. Adding CQRS pattern, mapper and extension class.

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,9 @@
+using API.Extensions;
+using Application.Activities;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,23 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy", policy =>
-    {
-        policy
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .WithOrigins("http://localhost:3001");
-    });
-});
 
+
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Application/Activities/List.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.Metadata;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities;
+
+public class List
+{
+    public class Query : IRequest<List<Activity>>{}
+    
+    public class Handler : IRequestHandler<Query, List<Activity>> 
+    {
+        private readonly DataContext _context;
+
+        public Handler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async System.Threading.Tasks.Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            return await _context.Activities.ToListAsync(cancellationToken: cancellationToken);        
+        }
+    }
+}
