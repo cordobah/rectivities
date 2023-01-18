7e044e9181fa954554a1cff500cec77d36ac60e9
Luis Cordoba <luis.cordoba@perficient.com>
2023-01-18 11:25:59 -0500

    End section 17. Client side image upload drop and crop image controls.

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,6 +4,7 @@ import {toast} from "react-toastify";
 import {router} from "../router/Routes";
 import {store} from "../stores/store";
 import {User, UserFormValues} from "../models/user";
+import {Photo, Profile} from "../models/profile";
 
 
 const sleep = (delay: number) => {
@@ -16,13 +17,13 @@ axios.defaults.baseURL = 'https://localhost:7000/api'
 
 axios.interceptors.request.use(config => {
     const token = store.commonStore.token;
-    if(token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+    if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
     return config;
 })
 
 axios.interceptors.response.use(async response => {
-        await sleep(500);
-        return response;
+    await sleep(500);
+    return response;
 }, (error: AxiosError) => {
     const {data, status, config} = error.response as AxiosResponse;
     switch (status) {
@@ -30,10 +31,10 @@ axios.interceptors.response.use(async response => {
             if (config.method === 'get' && data.errors && data.errors.hasOwnProperty('id')) {
                 router.navigate('/not-found');
             }
-            if (data.errors){
+            if (data.errors) {
                 const modalStateErrors = [];
-                for(const key in data.errors){
-                    if (data.errors[key]){
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
                         modalStateErrors.push(data.errors[key])
                     }
                 }
@@ -61,20 +62,20 @@ axios.interceptors.response.use(async response => {
     return Promise.reject(error);
 })
 
-const responseBody = <T> (response: AxiosResponse<T>) => response.data;
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-    get: <T> (url: string) => axios.get<T>(url).then(responseBody),
-    post: <T> (url: string, body: {}) => axios.post<T>(url,body).then(responseBody),
-    put: <T> (url: string, body: {}) => axios.put<T>(url,body).then(responseBody),
-    del: <T> (url: string) => axios.delete<T>(url).then(responseBody)
+    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    del: <T>(url: string) => axios.delete<T>(url).then(responseBody)
 }
 
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: ActivityFormValues) => requests.post<void>('/activities',activity),
-    update: (activity: ActivityFormValues) => requests.put<void>(`/activities/${activity?.id}`,activity),
+    create: (activity: ActivityFormValues) => requests.post<void>('/activities', activity),
+    update: (activity: ActivityFormValues) => requests.put<void>(`/activities/${activity?.id}`, activity),
     delete: (id: string) => requests.del<void>(`/activities/${id}`),
     attend: (id: string) => requests.post<void>(`/activities/${id}/attend`, {})
 }
@@ -85,9 +86,23 @@ const Account = {
     register: (user: UserFormValues) => requests.post<User>('/account/register', user)
 }
 
+const Profiles = {
+    get: (username: string) => requests.get<Profile>(`/profiles/${username}`),
+    uploadPhoto: (file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post<Photo>('photos', formData, {
+            headers: {'Content-Type': 'multipart/form-dat'}
+        })
+    },
+    setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
+}
+
 const agent = {
     Activities,
-    Account
+    Account,
+    Profiles
 }
 
 export default agent;
