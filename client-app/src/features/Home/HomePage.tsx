d8f0eb645f384fd70a2c57ccf42da229cc6fcf88
Luis Cordoba <luis.cordoba@perficient.com>
2023-01-24 10:48:57 -0500

    Section 22: Update initial message

--- client-app/src/features/Home/HomePage.tsx
+++ client-app/src/features/Home/HomePage.tsx
@@ -18,7 +18,7 @@ export default observer(function HomePage() {
                 </Header>
                 {userStore.isLoggedIn ? (
                     <>
-                        <Header as='h2' inverted content='Welcome to Reactivities'/>
+                        <Header as='h2' inverted content={'Welcome back '+ userStore.user?.displayName}/>
                         <Button as={Link} to='/activities' size='huge' inverted>
                             Got to Activities!
                         </Button>
